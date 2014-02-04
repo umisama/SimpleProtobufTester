83b9c4f094505d8c240e4d3fb10c6d094afb151b
umisama <umisama@fe2o3.jp>
2014-02-04 16:57:20 +0900

    complete implement

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"flag"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"text/template"
@@ -11,46 +13,91 @@ const (
 	TEMP_SCRIPT_NAME = "tempscript_protobuf.go"
 )
 
+var (
+	flagProtobufFileName = flag.String("p", "types.proto", "Specify path to proto-buf file.")
+	flagJsonFileName     = flag.String("j", "types.json", "Specify path to json file that including content data.")
+	flagPathUri          = flag.String("u", "http://localhost:8001", "Specify target uri.")
+	flagRootType         = flag.String("r", "", "[MUST]Root type name")
+)
+
 type Template struct {
-	Echo string
+	RootType string
+	JsonData string
+	Endpoint string
 }
 
 func main() {
-	err := CreateProtobuf("")
+	flag.Parse()
+	if *flagRootType == "" {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	err := wMain()
 	if err != nil {
 		os.Exit(2)
+	}
+
+	os.Exit(0)
+}
+
+func wMain() (err error) {
+	err = CreateProtobuf("")
+	if err != nil {
+		return
 	}
 
 	code, err := CreateScript()
 	if err != nil {
-		os.Exit(2)
+		return
 	}
 
 	err = WriteScript(code)
+	defer func() { err = os.Remove(TEMP_SCRIPT_NAME) }()
 	if err != nil {
-		os.Exit(2)
+		return
 	}
-	println(code)
 
 	err = DoScript()
-	if err != nil {
-		os.Exit(2)
-	}
+	return
 }
 
-func CreateProtobuf(filepath string) error {
-	return nil
+func CreateProtobuf(filepath string) (err error) {
+	cmd := exec.Command("protoc", "--go_out=.", *flagProtobufFileName)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	return
 }
 
 func CreateScript() (code string, err error) {
-	t := template.Must(template.New("script").Parse(code_template))
+	f, err := os.Open(*flagJsonFileName)
+	if err != nil {
+		return
+	}
 
+	json, err := ioutil.ReadAll(f)
+	if err != nil {
+		return
+	}
+
+	t := template.Must(template.ParseFiles("runner.go.tmpl"))
 	buf := bytes.NewBuffer([]byte{})
+	t_dat := Template{
+		RootType: *flagRootType,
+		JsonData: string(json),
+		Endpoint: *flagPathUri,
+	}
 
-	dat := Template{Echo: "hellohello from code"}
+	err = t.Execute(buf, t_dat)
+	if err != nil {
+		return
+	}
 
-	t.Execute(buf, dat)
-	return buf.String(), nil
+	code = buf.String()
+
+	return
 }
 
 func WriteScript(code string) (err error) {
@@ -64,7 +111,7 @@ func WriteScript(code string) (err error) {
 }
 
 func DoScript() (err error) {
-	cmd := exec.Command("go", "run", TEMP_SCRIPT_NAME)
+	cmd := exec.Command("go", "run", TEMP_SCRIPT_NAME, "types.pb.go")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -73,16 +120,5 @@ func DoScript() (err error) {
 		return
 	}
 
-	err = os.Remove(TEMP_SCRIPT_NAME)
 	return
 }
-
-var code_template = `package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Printf("{{.Echo}}")
-}`
